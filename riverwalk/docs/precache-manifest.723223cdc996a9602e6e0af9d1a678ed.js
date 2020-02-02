d48dfe7b88bff0a761941c0b204b8b0fd86b35ce
Andres Quan <aquan@Andress-MacBook-Pro.local>
2020-02-02 10:33:08 -0700

    Add production build

---
+++ riverwalk/docs/precache-manifest.723223cdc996a9602e6e0af9d1a678ed.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2db2e5a3e95ccd8d6e4f05969242cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5308909a6420a8bd850",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "704e79bb966db557219f",
+    "url": "/static/css/main.031e59c0.chunk.css"
+  },
+  {
+    "revision": "d5308909a6420a8bd850",
+    "url": "/static/js/2.f30edf73.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.f30edf73.chunk.js.LICENSE"
+  },
+  {
+    "revision": "704e79bb966db557219f",
+    "url": "/static/js/main.092019fe.chunk.js"
+  },
+  {
+    "revision": "51afb0f9405b14042c9d",
+    "url": "/static/js/runtime-main.4b390816.js"
+  },
+  {
+    "revision": "c1db66507c1082e9d8810b6b56d9b5b3",
+    "url": "/static/media/RiverwalkLogo.c1db6650.png"
+  },
+  {
+    "revision": "736c9ada64c675e74d5de360a9534a77",
+    "url": "/static/media/background.736c9ada.jpg"
+  },
+  {
+    "revision": "7584c8f0fd6cf8e45ab4b7c4003065bb",
+    "url": "/static/media/cigna-logo.7584c8f0.png"
+  },
+  {
+    "revision": "bea0d97163377269153e397b5a873f0d",
+    "url": "/static/media/claudia-with-lilly-cropped.bea0d971.jpg"
+  },
+  {
+    "revision": "303316adade0854fbf89404d3362d2d8",
+    "url": "/static/media/riverwalk-bw.303316ad.jpg"
+  }
+]);
