a5189dcd91262b50d2816613eb70bdf5c46370f0
Andres Quan <aquan@Andress-MacBook-Pro.local>
2020-02-08 14:47:15 -0700

    Add Care Credit as an accepted insurance

--- riverwalk/src/Insurance.js
+++ riverwalk/src/Insurance.js
@@ -4,11 +4,13 @@ import './Insurance.css';
 import delta from './pictures/DeltaDental.png';
 import cigna from './pictures/cigna-logo.png';
 import united from './pictures/UnitedConcordiaLogo.png';
+import careCredit from './pictures/care-credit.png';
 
 function Insurance() {
   return (
     <div className="Insurance">
 	<ul> <u><b>Insurances Accepted</b></u>
+		<li> Care Credit <img className="insurance-logo" src={careCredit}/></li>
 		<li> Cigna <img className="insurance-logo" src={cigna}/></li>
 		<li> Delta Dental <img className="insurance-logo" src={delta}/></li>
 		<li> United Concordia <img className="insurance-logo" src={united}/></li>
